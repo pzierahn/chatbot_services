84dea249cd8ed54ecc7b5d903279bd780f72a03d
patrick <patrick.zierahn@student.kit.edu>
2023-08-23 16:13:11 +0200

    Simplified pdf read

--- pdf/pdf_read.go
+++ pdf/pdf_read.go
@@ -2,44 +2,24 @@ package pdf
 
 import (
 	"context"
-	"fmt"
 	"os/exec"
+	"strings"
 )
 
-// ReadFile reads a PDF file and returns its contents as a byte slice.
-func ReadFile(ctx context.Context, filename string) (result []byte, err error) {
-	cmd := exec.CommandContext(ctx, "pdftotext", filename, "-")
-	return cmd.CombinedOutput()
-}
-
-// ReadFileString reads a PDF file and returns its contents as a string.
-func ReadFileString(ctx context.Context, filename string) (result string, err error) {
-	data, err := ReadFile(ctx, filename)
-	if err != nil {
-		return "", err
-	}
-
-	return string(data), nil
-}
-
 // ReadPages reads a PDF file and returns its contents as a slice of strings.
-func ReadPages(ctx context.Context, filename string, pages int) (result []string, err error) {
-	for page := 1; page <= pages; page++ {
-
-		cmd := exec.CommandContext(ctx, "pdftotext",
-			"-f", fmt.Sprint(page),
-			"-l", fmt.Sprint(page),
-			"-layout",
-			filename,
-			"-",
-		)
-		data, err := cmd.CombinedOutput()
-		if err != nil {
-			return nil, err
-		}
-
-		result = append(result, string(data))
+func ReadPages(ctx context.Context, filename string) (result []string, err error) {
+	cmd := exec.CommandContext(ctx, "pdftotext",
+		"-layout",
+		"-htmlmeta",
+		filename,
+		"-",
+	)
+	data, err := cmd.CombinedOutput()
+	if err != nil {
+		return nil, err
 	}
 
-	return result, nil
+	text := string(data)
+
+	return strings.Split(text, "\f"), nil
 }
