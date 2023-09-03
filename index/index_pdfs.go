3d286f801396d32aba2ca57fae9d6df6ecf9bde1
patrick <patrick.zierahn@student.kit.edu>
2023-09-03 12:17:03 +0200

    Update index package

--- index/index_pdfs.go
+++ index/index_pdfs.go
@@ -2,13 +2,10 @@ package index
 
 import (
 	"context"
-	"github.com/google/uuid"
 	"github.com/pzierahn/braingain/database"
 	"github.com/pzierahn/braingain/pdf"
-	pb "github.com/qdrant/go-client/qdrant"
 	"github.com/sashabaranov/go-openai"
 	"log"
-	"os"
 	"path/filepath"
 	"strings"
 )
@@ -22,6 +19,14 @@ func (index Index) File(ctx context.Context, filename string) {
 	}
 
 	log.Printf("--> Pages: %d\n", len(pages))
+
+	// Create document
+	docId, err := index.conn.CreateDocument(ctx, database.Document{
+		Filename: filepath.Base(filename),
+	})
+	if err != nil {
+		log.Fatalf("could not create document: %v", err)
+	}
 
 	for inx, page := range pages {
 		log.Printf("--> %d/%d\n", inx+1, len(pages))
@@ -43,54 +48,15 @@ func (index Index) File(ctx context.Context, filename string) {
 			log.Fatalf("could not create embeddings: %v", err)
 		}
 
-		err = index.conn.Upsert(ctx, database.Payload{
-			Uuid:       uuid.NewString(),
-			Collection: index.collection,
-			Data:       resp.Data[0].Embedding,
-			Metadata: map[string]*pb.Value{
-				"filename": {
-					Kind: &pb.Value_StringValue{
-						StringValue: filepath.Base(filename),
-					},
-				},
-				"page": {
-					Kind: &pb.Value_IntegerValue{
-						IntegerValue: int64(inx),
-					},
-				},
-				"content": {
-					Kind: &pb.Value_StringValue{
-						StringValue: page,
-					},
-				},
-			},
+		_, err = index.conn.Upsert(ctx, database.Point{
+			Source:    docId,
+			Page:      inx,
+			Text:      page,
+			Embedding: resp.Data[0].Embedding,
 		})
 
 		if err != nil {
 			log.Fatalf("could not upsert points: %v", err)
 		}
-	}
-}
-
-func (index Index) Files(ctx context.Context, baseDir string) {
-	_ = index.conn.DeleteCollection(ctx, index.collection)
-	err := index.conn.CreateCollection(ctx, index.collection, 1536, pb.Distance_Cosine)
-	if err != nil {
-		log.Fatalf("could not create collection: %v", err)
-	}
-
-	// Read PDF files in baseDir
-	files, err := os.ReadDir(baseDir)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for _, file := range files {
-		if !strings.HasSuffix(file.Name(), ".pdf") {
-			continue
-		}
-
-		pdfFile := baseDir + "/" + file.Name()
-		index.File(ctx, pdfFile)
 	}
 }
