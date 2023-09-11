21de17f20bf3e9355be0bc943ca4bdc56573c113
patrick <patrick.zierahn@student.kit.edu>
2023-09-11 08:25:09 +0200

    Added augment functionality when prompt documents are missing

--- server/server_chat.go
+++ server/server_chat.go
@@ -9,9 +9,12 @@ import (
 	"strings"
 )
 
-func (server *Server) Chat(ctx context.Context, prompt *pb.Prompt) (*pb.Completion, error) {
-	log.Printf("Chat: %v", prompt)
+type background struct {
+	text []string
+	docs []*pb.Completion_Document
+}
 
+func (server *Server) getBackgroundFromPrompt(ctx context.Context, prompt *pb.Prompt) (*background, error) {
 	sort.Slice(prompt.Documents, func(i, j int) bool {
 		return prompt.Documents[i].Filename < prompt.Documents[j].Filename
 	})
@@ -22,7 +25,7 @@ func (server *Server) Chat(ctx context.Context, prompt *pb.Prompt) (*pb.Completi
 		})
 	}
 
-	var background []string
+	var text []string
 	var sources []*pb.Completion_Document
 
 	for _, doc := range prompt.Documents {
@@ -41,7 +44,7 @@ func (server *Server) Chat(ctx context.Context, prompt *pb.Prompt) (*pb.Completi
 			parts = append(parts, page.Text)
 		}
 
-		background = append(background, strings.Join(parts, "\n"))
+		text = append(text, strings.Join(parts, "\n"))
 		sources = append(sources, &pb.Completion_Document{
 			Id:       doc.Id,
 			Filename: doc.Filename,
@@ -49,7 +52,69 @@ func (server *Server) Chat(ctx context.Context, prompt *pb.Prompt) (*pb.Completi
 		})
 	}
 
-	response, err := server.chat.Chat(ctx, prompt.Prompt, background)
+	return &background{
+		text: text,
+		docs: sources,
+	}, nil
+}
+
+func (server *Server) getBackgroundFromDB(ctx context.Context, prompt *pb.Prompt) (*background, error) {
+
+	results, err := server.chat.Search(ctx, prompt.Prompt)
+	if err != nil {
+		return nil, err
+	}
+
+	text := make([]string, len(results))
+	pages := make(map[uuid.UUID][]uint32)
+	scores := make(map[uuid.UUID][]float32)
+
+	for inx, result := range results {
+		text[inx] = result.Text
+
+		source := result.Source
+		pages[source] = append(pages[source], uint32(result.Page))
+		scores[source] = append(scores[source], result.Score)
+	}
+
+	var sources []*pb.Completion_Document
+	for source := range pages {
+		doc, err := server.db.GetDocument(ctx, source)
+		if err != nil {
+			return nil, err
+		}
+
+		sources = append(sources, &pb.Completion_Document{
+			Id:       doc.Id.String(),
+			Filename: doc.Filename,
+			Pages:    pages[source],
+			Scores:   scores[source],
+		})
+	}
+
+	return &background{
+		text: text,
+		docs: sources,
+	}, nil
+}
+
+func (server *Server) Chat(ctx context.Context, prompt *pb.Prompt) (*pb.Completion, error) {
+	log.Printf("Chat: %v", prompt)
+
+	var bg *background
+	var err error
+
+	if prompt.Documents == nil || len(prompt.Documents) == 0 {
+		bg, err = server.getBackgroundFromDB(ctx, prompt)
+	} else {
+		bg, err = server.getBackgroundFromPrompt(ctx, prompt)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	response, err := server.chat.Chat(ctx, prompt.Prompt, bg.text)
 	if err != nil {
 		return nil, err
 	}
@@ -57,7 +122,7 @@ func (server *Server) Chat(ctx context.Context, prompt *pb.Prompt) (*pb.Completi
 	completion := &pb.Completion{
 		Prompt:    prompt,
 		Text:      response.Completion,
-		Documents: sources,
+		Documents: bg.docs,
 	}
 
 	return completion, nil
