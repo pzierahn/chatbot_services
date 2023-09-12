b17575637801258d58664593b67666a09dbaaa1f
patrick <patrick.zierahn@student.kit.edu>
2023-09-12 13:48:39 +0200

    Create embedding concurrently

--- index/index_embeddings.go
+++ index/index_embeddings.go
@@ -2,37 +2,59 @@ package index
 
 import (
 	"context"
+	"errors"
 	"github.com/pzierahn/braingain/database"
 	"github.com/sashabaranov/go-openai"
+	"log"
 	"strings"
+	"sync"
 )
 
 func (index Index) GetPagesWithEmbeddings(ctx context.Context, pages []string) ([]*database.PageEmbedding, error) {
+	var mu sync.Mutex
 	var embeddings []*database.PageEmbedding
+	var errs []error
+
+	var wg sync.WaitGroup
+	wg.Add(len(pages))
 
 	for inx, page := range pages {
-		page = strings.TrimSpace(page)
-		if len(page) == 0 {
-			continue
-		}
+		go func(inx int, page string) {
+			defer wg.Done()
 
-		resp, err := index.GPT.CreateEmbeddings(
-			ctx,
-			openai.EmbeddingRequestStrings{
-				Model: openai.AdaEmbeddingV2,
-				Input: []string{page},
-			},
-		)
-		if err != nil {
-			return nil, err
-		}
+			log.Printf("Processing page %v", inx)
+			page = strings.TrimSpace(page)
+			if len(page) == 0 {
+				return
+			}
 
-		embeddings = append(embeddings, &database.PageEmbedding{
-			Page:      inx,
-			Text:      page,
-			Embedding: resp.Data[0].Embedding,
-		})
+			resp, err := index.GPT.CreateEmbeddings(
+				ctx,
+				openai.EmbeddingRequestStrings{
+					Model: openai.AdaEmbeddingV2,
+					Input: []string{page},
+				},
+			)
+
+			log.Printf("--> Processed page %v", inx)
+
+			mu.Lock()
+			defer mu.Unlock()
+
+			if err != nil {
+				errs = append(errs, err)
+				return
+			}
+
+			embeddings = append(embeddings, &database.PageEmbedding{
+				Page:      inx,
+				Text:      page,
+				Embedding: resp.Data[0].Embedding,
+			})
+		}(inx, page)
 	}
 
-	return embeddings, nil
+	wg.Wait()
+
+	return embeddings, errors.Join(errs...)
 }
