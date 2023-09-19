c20f76fbd1b8d8a9ff73049903fa187cebc59c9e
patrick <patrick.zierahn@student.kit.edu>
2023-09-20 00:26:34 +0200

    Reformat SQL

--- database/database_usage.go
+++ database/database_usage.go
@@ -18,7 +18,9 @@ type Usage struct {
 
 // CreateUsage inserts a new usage record into the openai_usage table
 func (client *Client) CreateUsage(ctx context.Context, usage Usage) error {
-	_, err := client.conn.Exec(ctx, "INSERT INTO openai_usage (id, uid, model, input, output) VALUES ($1, $2, $3, $4, $5)",
+	_, err := client.conn.Exec(ctx,
+		`INSERT INTO openai_usage (id, uid, model, input, output)
+			VALUES ($1, $2, $3, $4, $5)`,
 		usage.ID, usage.UID, usage.Model, usage.Input, usage.Output)
 	return err
 }
@@ -26,20 +28,25 @@ func (client *Client) CreateUsage(ctx context.Context, usage Usage) error {
 // ReadUsage retrieves a usage record by ID from the openai_usage table
 func (client *Client) ReadUsage(ctx context.Context, id string) (Usage, error) {
 	var usage Usage
-	err := client.conn.QueryRow(ctx, "SELECT id, created_at, uid, model, input, output FROM openai_usage WHERE id = $1", id).
+	err := client.conn.QueryRow(ctx,
+		`SELECT id, created_at, uid, model, input, output
+			FROM openai_usage WHERE id = $1`, id).
 		Scan(&usage.ID, &usage.CreatedAt, &usage.UID, &usage.Model, &usage.Input, &usage.Output)
 	return usage, err
 }
 
 // UpdateUsage updates an existing usage record in the openai_usage table
 func (client *Client) UpdateUsage(ctx context.Context, usage Usage) error {
-	_, err := client.conn.Exec(ctx, "UPDATE openai_usage SET uid = $2, model = $3, input = $4, output = $5 WHERE id = $1",
+	_, err := client.conn.Exec(ctx,
+		`UPDATE openai_usage
+			SET uid = $2, model = $3, input = $4, output = $5
+			WHERE id = $1`,
 		usage.ID, usage.UID, usage.Model, usage.Input, usage.Output)
 	return err
 }
 
 // DeleteUsage deletes a usage record by ID from the openai_usage table
 func (client *Client) DeleteUsage(ctx context.Context, id string) error {
-	_, err := client.conn.Exec(ctx, "DELETE FROM openai_usage WHERE id = $1", id)
+	_, err := client.conn.Exec(ctx, `DELETE FROM openai_usage WHERE id = $1`, id)
 	return err
 }
