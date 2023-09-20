afe6eaa2ad49747b02b85d22160bde5c1a5d58ea
patrick <patrick.zierahn@student.kit.edu>
2023-09-20 14:53:06 +0200

    Rename some table columns

--- database/db_setup.sql
+++ database/db_setup.sql
@@ -7,20 +7,20 @@ create table if not exists collections
 
 create table if not exists documents
 (
-    id         uuid primary key default gen_random_uuid(),
-    uid        text not null,
-    filename   text not null,
-    path       text not null,
-    collection uuid references collections (id) ON DELETE CASCADE
+    id            uuid primary key default gen_random_uuid(),
+    uid           text not null,
+    filename      text not null,
+    path          text not null,
+    collection_id uuid references collections (id) ON DELETE CASCADE
 );
 
 create table if not exists document_embeddings
 (
-    id        uuid primary key default gen_random_uuid(),
-    source    uuid references documents (id) ON DELETE CASCADE,
-    page      integer not null,
-    text      text    not null,
-    embedding vector(1536)
+    id          uuid primary key default gen_random_uuid(),
+    document_id uuid references documents (id) ON DELETE CASCADE,
+    page        integer not null,
+    text        text    not null,
+    embedding   vector(1536)
 );
 
 CREATE INDEX IF NOT EXISTS hnsw_index ON document_embeddings USING hnsw (embedding vector_ip_ops);
@@ -28,8 +28,8 @@ CREATE INDEX IF NOT EXISTS hnsw_index ON document_embeddings USING hnsw (embeddi
 create table if not exists openai_usage
 (
     id         uuid primary key   default gen_random_uuid(),
-    created_at timestamp not null default now(),
     uid        text      not null,
+    created_at timestamp not null default now(),
     model      text      not null,
     input      int       not null,
     output     int       not null
@@ -37,17 +37,17 @@ create table if not exists openai_usage
 
 create table if not exists chat_message
 (
-    id         uuid primary key   default gen_random_uuid(),
-    uid        text      not null,
-    created_at timestamp not null default now(),
-    collection uuid      not null references collections (id) ON DELETE CASCADE,
-    prompt     text      not null,
-    completion text      not null
+    id            uuid primary key   default gen_random_uuid(),
+    uid           text      not null,
+    created_at    timestamp not null default now(),
+    collection_id uuid      not null references collections (id) ON DELETE CASCADE,
+    prompt        text      not null,
+    completion    text      not null
 );
 
 create table if not exists chat_message_source
 (
-    id            uuid primary key default gen_random_uuid(),
-    chat          uuid not null references chat_message (id) ON DELETE CASCADE,
-    document_page uuid not null references document_embeddings (id) ON DELETE CASCADE
+    id              uuid primary key default gen_random_uuid(),
+    chat_message_id uuid not null references chat_message (id) ON DELETE CASCADE,
+    document_page   uuid not null references document_embeddings (id) ON DELETE CASCADE
 );
