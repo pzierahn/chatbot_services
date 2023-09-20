45bd9712bf34c90110e9a0bc34cae40acbc317ce
patrick <patrick.zierahn@student.kit.edu>
2023-09-20 08:28:13 +0200

    Refactor

--- database/database_setup.go
+++ database/database_setup.go
@@ -5,12 +5,12 @@ import (
 	_ "embed"
 )
 
-//go:embed create_tables.sql
-var tablesSQL string
+//go:embed db_setup.sql
+var dbSetup string
 
 // SetupTables creates all the tables in the database
 func (client *Client) SetupTables(ctx context.Context) error {
-	_, err := client.conn.Exec(ctx, tablesSQL)
+	_, err := client.conn.Exec(ctx, dbSetup)
 
 	return err
 }
