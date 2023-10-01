3121f17b6e7883697dce37d0dc1c209286a2cfaf
patrick <patrick.zierahn@student.kit.edu>
2023-10-01 13:06:15 +0200

    Added reporting to CollectionCreate tests

--- test/test_collections.go
+++ test/test_collections.go
@@ -1,6 +1,8 @@
 package test
 
 import (
+	"context"
+	"fmt"
 	pb "github.com/pzierahn/brainboost/proto"
 	"google.golang.org/protobuf/types/known/emptypb"
 	"log"
@@ -11,31 +13,40 @@ func (setup *Setup) CollectionCreate() {
 	ctx, userId := setup.createRandomSignIn()
 	defer setup.DeleteUser(userId)
 
-	coll, err := setup.collections.Create(ctx, &pb.Collection{
-		Name: "Test Collection",
+	setup.report.ExpectError("collection_create_without_auth", func() error {
+		_, err := setup.collections.Create(context.Background(), &pb.Collection{
+			Name: "Test Collection",
+		})
+		return err
 	})
-	if err != nil {
-		log.Fatal(err)
-	}
 
-	colls, err := setup.collections.GetAll(ctx, &emptypb.Empty{})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for _, c := range colls.Items {
-		if c.Id != coll.Id {
-			continue
+	setup.report.Run("collection_create", func() error {
+		coll, err := setup.collections.Create(ctx, &pb.Collection{
+			Name: "Test Collection",
+		})
+		if err != nil {
+			return err
 		}
 
-		log.Println("Collection found:", c.Name)
-
-		if c.Name != coll.Name {
-			log.Fatal("Collection name mismatch")
+		colls, err := setup.collections.GetAll(ctx, &emptypb.Empty{})
+		if err != nil {
+			return err
 		}
 
-		break
-	}
+		if len(colls.Items) != 1 {
+			return fmt.Errorf("collection not created")
+		}
+
+		if colls.Items[0].Id != coll.Id {
+			return fmt.Errorf("collection id mismatch")
+		}
+
+		if colls.Items[0].Name != coll.Name {
+			return fmt.Errorf("collection name mismatch")
+		}
+
+		return nil
+	})
 }
 
 func (setup *Setup) CollectionRename() {
