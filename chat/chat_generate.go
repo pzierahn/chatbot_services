d8cfcaf76804f50da5551243afefced940d174e2
patrick <patrick.zierahn@student.kit.edu>
2023-12-15 19:38:09 +0100

    Delete ChatWithDocuments

--- chat/chat_generate.go
+++ chat/chat_generate.go
@@ -29,95 +29,13 @@ func (service *Service) Chat(ctx context.Context, prompt *pb.Prompt) (*pb.ChatMe
 		return nil, account.NoFundingError()
 	}
 
-	chunkIds, fragments, err := service.searchForContext(ctx, prompt)
-	if err != nil {
-		return nil, err
+	var chunkIds, fragments []string
+
+	if len(prompt.Documents) > 0 {
+		chunkIds, fragments, err = service.searchForContext(ctx, prompt)
+	} else {
+		chunkIds, fragments, err = service.getDocumentsContext(ctx, userId, prompt)
 	}
-
-	messages := []openai.ChatCompletionMessage{
-		{
-			Role:    openai.ChatMessageRoleSystem,
-			Content: "Answer in Markdown format without any code blocks",
-		},
-	}
-
-	for _, text := range fragments {
-		messages = append(messages, openai.ChatCompletionMessage{
-			Role:    openai.ChatMessageRoleUser,
-			Content: text,
-		})
-	}
-
-	messages = append(messages, openai.ChatCompletionMessage{
-		Role:    openai.ChatMessageRoleUser,
-		Content: prompt.Prompt,
-	})
-
-	resp, err := service.gpt.CreateChatCompletion(
-		ctx,
-		openai.ChatCompletionRequest{
-			Model:       prompt.ModelOptions.Model,
-			Temperature: prompt.ModelOptions.Temperature,
-			MaxTokens:   int(prompt.ModelOptions.MaxTokens),
-			Messages:    messages,
-			N:           1,
-			User:        userId,
-		},
-	)
-	if err != nil {
-		return nil, err
-	}
-
-	_, err = service.account.CreateUsage(ctx, account.Usage{
-		UserId: userId,
-		Model:  resp.Model,
-		Input:  uint32(resp.Usage.PromptTokens),
-		Output: uint32(resp.Usage.CompletionTokens),
-	})
-	if err != nil {
-		log.Printf("Chat: error %v", err)
-	}
-
-	completion := &pb.ChatMessage{
-		Id:           uuid.NewString(),
-		CollectionId: prompt.CollectionId,
-		Prompt:       prompt.Prompt,
-		Text:         resp.Choices[0].Message.Content,
-		References:   chunkIds,
-	}
-
-	_ = service.storeChatMessage(ctx, chatMessage{
-		id:           completion.Id,
-		userId:       userId,
-		collectionId: prompt.CollectionId,
-		prompt:       prompt.Prompt,
-		completion:   completion.Text,
-		references:   chunkIds,
-	})
-
-	return completion, nil
-}
-
-func (service *Service) ChatWithDocuments(ctx context.Context, prompt *pb.DocumentsPrompt) (*pb.ChatMessage, error) {
-	userId, err := service.auth.ValidateToken(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	if prompt.ModelOptions == nil {
-		return nil, fmt.Errorf("options missing")
-	}
-
-	funding, err := service.account.HasFunding(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	if !funding {
-		return nil, account.NoFundingError()
-	}
-
-	chunkIds, fragments, err := service.getDocumentsContext(ctx, userId, prompt)
 	if err != nil {
 		return nil, err
 	}
