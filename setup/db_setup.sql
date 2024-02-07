78d51a5630413875a6d99fabea090e784d9dad6b
patrick <patrick.zierahn@student.kit.edu>
2024-02-07 15:50:06 +0100

    Changed db scheme to handle threads

--- setup/db_setup.sql
+++ setup/db_setup.sql
@@ -32,7 +32,7 @@ create table if not exists openai_usages
     output_tokens int         not null
 );
 
-create table if not exists chat_messages
+create table if not exists threads
 (
     id            uuid primary key     default gen_random_uuid(),
     user_id       VARCHAR(36) not null,
@@ -42,10 +42,20 @@ create table if not exists chat_messages
     completion    text        not null
 );
 
-create table if not exists chat_message_references
+create table if not exists thread_messages
+(
+    id         uuid primary key     default gen_random_uuid(),
+    user_id    VARCHAR(36) not null,
+    thread_id  uuid        not null references threads (id) ON DELETE CASCADE,
+    created_at timestamp   not null default now(),
+    prompt     text        not null,
+    completion text        not null
+);
+
+create table if not exists thread_references
 (
     id                uuid primary key default gen_random_uuid(),
-    chat_message_id   uuid not null references chat_messages (id) ON DELETE CASCADE,
+    thread_id         uuid not null references threads (id) ON DELETE CASCADE,
     document_chunk_id uuid not null references document_chunks (id) ON DELETE CASCADE
 );
 
