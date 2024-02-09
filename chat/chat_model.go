a8d67b83562d94c702aaed8c8f57e1642355a49e
patrick <patrick.zierahn@student.kit.edu>
2024-02-09 20:30:01 +0100

    Added gpt-4-turbo-preview

--- chat/chat_model.go
+++ chat/chat_model.go
@@ -9,6 +9,8 @@ func (service *Service) getModel(name string) (llm.Completion, error) {
 	switch name {
 	case "gpt-4-1106-preview":
 		return service.openai, nil
+	case "gpt-4-turbo-preview":
+		return service.openai, nil
 	case "gpt-3.5-turbo-16k":
 		return service.openai, nil
 	case "gemini-pro":
