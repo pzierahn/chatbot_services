faf6a5ac0e0e05c12082d0ab61bf141c5c0e2297
patrick <patrick.zierahn@student.kit.edu>
2024-02-17 18:42:49 +0100

    Added a list function

---
+++ documents/docs_list.go
@@ -0,0 +1,49 @@
+package documents
+
+import (
+	"context"
+	pb "github.com/pzierahn/chatbot_services/proto"
+)
+
+func (service *Service) List(ctx context.Context, req *pb.DocumentFilter) (*pb.DocumentList, error) {
+
+	userId, err := service.auth.Verify(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	rows, err := service.db.Query(ctx,
+		`SELECT id, title
+		FROM documents
+		WHERE
+		    user_id = $1 AND
+		    collection_id = $2::uuid AND
+		    title LIKE $3`,
+		userId, req.CollectionId, "%"+req.Query+"%")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	documents := &pb.DocumentList{}
+
+	for rows.Next() {
+		var (
+			docId string
+			title string
+		)
+
+		err = rows.Scan(
+			&docId,
+			&title,
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		documents.Ids = append(documents.Ids, docId)
+		documents.Name = append(documents.Name, title)
+	}
+
+	return documents, nil
+}
