32f499ea5e875f059283ec749905213a07936971
patrick <patrick.zierahn@student.kit.edu>
2024-02-22 10:31:17 +0100

    Added title util

---
+++ utils/document_title.go
@@ -0,0 +1,14 @@
+package utils
+
+import pb "github.com/pzierahn/chatbot_services/proto"
+
+func GetDocumentTitle(meta *pb.DocumentMetadata) string {
+	switch meta.Data.(type) {
+	case *pb.DocumentMetadata_Web:
+		return meta.GetWeb().Title
+	case *pb.DocumentMetadata_File:
+		return meta.GetFile().Filename
+	default:
+		return ""
+	}
+}
