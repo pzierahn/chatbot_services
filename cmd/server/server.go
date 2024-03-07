6067f940b1e9812b3369163fafa74cd8e173d425
patrick <patrick.zierahn@student.kit.edu>
2024-03-07 19:56:40 +0100

    Added anthropic

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -11,6 +11,7 @@ import (
 	"github.com/pzierahn/chatbot_services/crashlytics"
 	"github.com/pzierahn/chatbot_services/documents"
 	"github.com/pzierahn/chatbot_services/llm"
+	"github.com/pzierahn/chatbot_services/llm/anthropic"
 	"github.com/pzierahn/chatbot_services/llm/bedrock"
 	"github.com/pzierahn/chatbot_services/llm/mistral"
 	"github.com/pzierahn/chatbot_services/llm/openai"
@@ -138,6 +139,11 @@ func main() {
 		log.Printf("failed to create mistral service: %v", err)
 	}
 
+	anthropicClient, err := anthropic.New(accountService)
+	if err != nil {
+		log.Fatalf("failed to create anthropic service: %v", err)
+	}
+
 	docsService := documents.FromConfig(&documents.Config{
 		Auth:       authService,
 		Account:    accountService,
@@ -161,6 +167,7 @@ func main() {
 			vertexService,
 			bedrockService,
 			mistralService,
+			anthropicClient,
 		},
 	})
 	pb.RegisterChatServiceServer(grpcServer, chatService)
