9c0a01f08aa5aa891f45b08f5e5b4562da65553a
patrick <patrick.zierahn@student.kit.edu>
2024-04-01 19:12:25 +0200

    Added Claude 3 Haiku

--- llm/bedrock/bedrock_models.go
+++ llm/bedrock/bedrock_models.go
@@ -8,6 +8,7 @@ import (
 const (
 	ClaudeV2Dot1 = "anthropic.claude-v2:1"
 	ClaudeSonnet = "anthropic.claude-3-sonnet-20240229-v1:0"
+	ClaudeHaiku  = "anthropic.claude-3-haiku-20240307-v1:0"
 )
 
 var ModelCosts = map[string]llm.PricePer1000Tokens{
@@ -19,6 +20,10 @@ var ModelCosts = map[string]llm.PricePer1000Tokens{
 		Input:  0.003,
 		Output: 0.015,
 	},
+	ClaudeHaiku: {
+		Input:  0.00025,
+		Output: 0.00125,
+	},
 }
 
 func (client *Client) ProvidesModel(name string) bool {
@@ -26,6 +31,8 @@ func (client *Client) ProvidesModel(name string) bool {
 	case name == ClaudeV2Dot1:
 		return true
 	case name == ClaudeSonnet:
+		return true
+	case name == ClaudeHaiku:
 		return true
 	case strings.HasPrefix(name, "amazon."):
 		return true
