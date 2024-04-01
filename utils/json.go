6c1fea654816180cbcd8ad7d9e61536a71420632
patrick <patrick.zierahn@student.kit.edu>
2024-04-01 23:23:54 +0200

    Update utils

--- utils/json.go
+++ utils/json.go
@@ -1,8 +1,24 @@
 package utils
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"log"
+	"os"
+)
 
 func Prettify(obj interface{}) string {
 	byt, _ := json.MarshalIndent(obj, "", "  ")
 	return string(byt)
+}
+
+func WriteJson(path string, obj interface{}) {
+	byt, err := json.MarshalIndent(obj, "", "  ")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = os.WriteFile(path, byt, 0644)
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
