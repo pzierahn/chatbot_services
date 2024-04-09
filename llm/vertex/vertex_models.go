7626a4b5c249e92525419d0e054c972d58a1ffa7
patrick <patrick.zierahn@student.kit.edu>
2024-04-09 23:22:49 +0200

    Update Model costs

--- llm/vertex/vertex_models.go
+++ llm/vertex/vertex_models.go
@@ -14,12 +14,12 @@ const (
 
 var ModelCosts = map[string]llm.PricePer1000Tokens{
 	GeminiPro: {
-		Input:  0.50 / 1_000_000,
-		Output: 1.50 / 1_000_000,
+		Input:  0.50 / 1_000,
+		Output: 1.50 / 1_000,
 	},
 	GeminiPro15: {
-		Input:  7 / 1_000_000,
-		Output: 21 / 1_000_000,
+		Input:  7 / 1_000,
+		Output: 21 / 1_000,
 	},
 }
 
