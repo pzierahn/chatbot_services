91efcabbeca34a4156b89ab8d0bac62afd66bc63
patrick <patrick.zierahn@student.kit.edu>
2024-04-28 12:19:36 +0200

    Added batch processing

--- chat/chat_batch.go
+++ chat/chat_batch.go
@@ -7,6 +7,22 @@ import (
 	pb "github.com/pzierahn/chatbot_services/proto"
 )
 
+type batchJob struct {
+	docIdx    uint32
+	promptIdx uint32
+	prompt    string
+	text      string
+	title     string
+	retry     int
+}
+
+type batchResults struct {
+	job        batchJob
+	completion string
+	err        error
+	retry      int
+}
+
 func getDocumentText(doc *pb.Document) string {
 	var text string
 
@@ -65,6 +81,10 @@ func (service *Service) BatchChat(ctx context.Context, req *pb.BatchRequest) (*p
 		response.PromptTitle[promptIdx] = resp.Text
 	}
 
+	scheduledJobs := 0
+	jobQueue := make(chan batchJob, len(req.DocumentIds))
+	defer close(jobQueue)
+
 	for docIdx, docID := range req.DocumentIds {
 		doc, err := service.docs.Get(ctx, &pb.DocumentID{Id: docID})
 		if err != nil {
@@ -75,26 +95,69 @@ func (service *Service) BatchChat(ctx context.Context, req *pb.BatchRequest) (*p
 		title := getDocumentTitle(doc)
 
 		for promptIdx, prompt := range req.Prompts {
-			completion, err := model.GenerateCompletion(ctx, &llm.GenerateRequest{
-				Model:       req.ModelOptions.Model,
-				MaxTokens:   int(req.ModelOptions.MaxTokens),
-				Temperature: req.ModelOptions.Temperature,
-				TopP:        req.ModelOptions.TopP,
-				UserId:      userId,
-				Messages: []*llm.Message{{
-					Text: text + "\n\n\n" + prompt,
-				}},
-			})
-			if err != nil {
-				return nil, err
+			jobQueue <- batchJob{
+				docIdx:    uint32(docIdx),
+				promptIdx: uint32(promptIdx),
+				prompt:    prompt,
+				text:      text,
+				title:     title,
+			}
+			scheduledJobs++
+		}
+	}
+
+	resultsQueue := make(chan batchResults, 4)
+	defer close(resultsQueue)
+
+	for worker := 0; worker < 4; worker++ {
+		go func() {
+			for job := range jobQueue {
+				completion, cerr := model.GenerateCompletion(ctx, &llm.GenerateRequest{
+					Model:       req.ModelOptions.Model,
+					MaxTokens:   int(req.ModelOptions.MaxTokens),
+					Temperature: req.ModelOptions.Temperature,
+					TopP:        req.ModelOptions.TopP,
+					UserId:      userId,
+					Messages: []*llm.Message{{
+						Text: job.text + "\n\n\n" + job.prompt,
+					}},
+				})
+				if cerr != nil {
+					resultsQueue <- batchResults{
+						job:   job,
+						err:   cerr,
+						retry: job.retry + 1,
+					}
+				} else {
+					resultsQueue <- batchResults{
+						job:        job,
+						completion: completion.Text,
+					}
+				}
+			}
+		}()
+	}
+
+	for result := range resultsQueue {
+		if result.err != nil {
+			if result.retry < 2 {
+				jobQueue <- result.job
+			} else {
+				return nil, result.err
 			}
 
-			response.Items = append(response.Items, &pb.BatchResponse_Completion{
-				DocumentId:    uint32(docIdx),
-				DocumentTitle: title,
-				Prompt:        uint32(promptIdx),
-				Completion:    completion.Text,
-			})
+			continue
+		}
+
+		response.Items = append(response.Items, &pb.BatchResponse_Completion{
+			DocumentId:    result.job.docIdx,
+			DocumentTitle: result.job.title,
+			Prompt:        result.job.promptIdx,
+			Completion:    result.completion,
+		})
+
+		if scheduledJobs == len(response.Items) {
+			break
 		}
 	}
 
