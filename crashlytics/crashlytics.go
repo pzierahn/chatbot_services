296bd9469c13c20ba0e733564e1fc03dffc718e4
patrick <patrick.zierahn@student.kit.edu>
2024-05-29 22:27:22 +0200

    Added some comments

--- crashlytics/crashlytics.go
+++ crashlytics/crashlytics.go
@@ -8,12 +8,14 @@ import (
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
+// Service implements the crashlytics service.
 type Service struct {
 	pb.UnimplementedCrashlyticsServiceServer
 	auth auth.Service
 	db   *pgxpool.Pool
 }
 
+// New creates a new crashlytics service.
 func New(auth auth.Service, db *pgxpool.Pool) *Service {
 	return &Service{
 		db:   db,
@@ -21,6 +23,7 @@ func New(auth auth.Service, db *pgxpool.Pool) *Service {
 	}
 }
 
+// RecordError records a frontend error in the database.
 func (server *Service) RecordError(ctx context.Context, req *pb.Error) (*emptypb.Empty, error) {
 	uid, err := server.auth.Verify(ctx)
 	if err != nil {
