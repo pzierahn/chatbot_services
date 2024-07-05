6d3b3c5af4e7c8d00a6ac6589d17fb7b2d4cdfd4
patrick <patrick.zierahn@student.kit.edu>
2024-07-06 00:49:22 +0200

    Update chat example

--- cmd/chat/chat.go
+++ cmd/chat/chat.go
@@ -2,36 +2,83 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"github.com/pzierahn/chatbot_services/llm"
 	"github.com/pzierahn/chatbot_services/llm/anthropic"
 	"log"
+	"strings"
 )
 
-// main demonstrates how to use the bedrock client to generate completions.
+func getSources(_ context.Context, input map[string]interface{}) (string, error) {
+	prompt, ok := input["prompt"].(string)
+	if !ok {
+		return "", fmt.Errorf("missing prompt")
+	}
+
+	log.Printf("Call get_sources: '%s'", prompt)
+
+	var sources []map[string]string
+
+	if strings.Contains(prompt, "Arnold Pitterson") {
+		sources = append(sources, map[string]string{
+			"SourceID": "S1",
+			"Content":  "Arnold Pitterson is a fictional character in the book 'The City of Glass' by Paul Auster.",
+		})
+	}
+
+	if strings.Contains(prompt, "Hugo Alberts von Tahl") {
+		sources = append(sources, map[string]string{
+			"SourceID": "S2",
+			"Content":  "Hugo Alberts von Tahl was a German philosopher who lived in the 19th century. He is known for his work on the philosophy of language and logic.",
+		})
+	}
+
+	byt, _ := json.Marshal(sources)
+	return string(byt), nil
+}
+
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
-	client, err := anthropic.New(llm.DummyTracker{
-		PrintUsage: true,
-	})
-	if err != nil {
-		log.Fatalf("Failed to create client: %v", err)
-	}
-
 	ctx := context.Background()
-	resp, err := client.GenerateCompletion(ctx, &llm.GenerateRequest{
-		Messages: []*llm.Message{{
-			Type: llm.MessageTypeUser,
-			Text: "What is the meaning of life?",
-		}},
-		Model:       anthropic.ClaudeSonnet35,
-		MaxTokens:   128,
-		TopP:        1.0,
-		Temperature: 1.0,
-	})
+
+	client, err := anthropic.New()
 	if err != nil {
-		log.Fatalf("Failed to generate completion: %v", err)
+		log.Fatal(err)
 	}
 
-	log.Printf("CompletionResponse: %v", resp.Text)
+	client.SetTools([]llm.ToolDefinition{{
+		Name:        "get_sources",
+		Description: "Retrieves the sources for the prompt. The prompt should be optimized for embedding retrieval. The tool will return a list of sources in JSON format with the following fields: SourceID, Content.",
+		Parameters: llm.ToolParameters{
+			Type: "object",
+			Properties: map[string]llm.ParametersProperties{
+				"prompt": {
+					Type:        "string",
+					Description: "The topic for which to retrieve sources. The prompt should be optimized for embedding retrieval.",
+				},
+			},
+			Required: []string{"prompt"},
+		},
+		Call: getSources,
+	}})
+
+	resp, err := client.Completion(ctx, &llm.CompletionRequest{
+		SystemPrompt: "You are a helpful assistant. Quote the sources by \\cite{SourceID}",
+		Messages: []*llm.Message{{
+			Role:    llm.MessageTypeUser,
+			Content: "Who are Arnold Pitterson and Hugo Alberts von Tahl?",
+		}},
+		Temperature: 1.0,
+		TopP:        1.0,
+		MaxTokens:   256,
+		Model:       anthropic.ClaudeSonnet35,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	byt, _ := json.MarshalIndent(resp, "", "  ")
+	log.Println(string(byt))
 }
