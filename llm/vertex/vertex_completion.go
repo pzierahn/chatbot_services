040ae3804678b16dcca3b24ce8c6ff57358fe2e8
patrick <patrick.zierahn@student.kit.edu>
2024-07-07 19:50:28 +0200

    Refactor transformToHistory

--- llm/vertex/vertex_completion.go
+++ llm/vertex/vertex_completion.go
@@ -14,66 +14,6 @@ const (
 	RoleModel = "model"
 )
 
-func (client *Client) transformToHistory(messages []*llm.Message) ([]*genai.Content, error) {
-	var history []*genai.Content
-
-	for inx, msg := range messages {
-		var role string
-
-		if msg.Role == llm.RoleUser {
-			role = RoleUser
-		} else {
-			role = RoleModel
-		}
-
-		if msg.Content != "" {
-			history = append(history, &genai.Content{
-				Role:  role,
-				Parts: []genai.Part{genai.Text(msg.Content)},
-			})
-		}
-
-		for iny, call := range msg.ToolCalls {
-			var args map[string]interface{}
-			err := json.Unmarshal([]byte(call.Function.Arguments), &args)
-			if err != nil {
-				return nil, err
-			}
-
-			history = append(history, &genai.Content{
-				Role: RoleModel,
-				Parts: []genai.Part{genai.FunctionCall{
-					Name: call.Function.Name,
-					Args: args,
-				}},
-			})
-
-			// Check if the next message is a tool response
-			if inx+1 < len(messages) && len(messages[inx+1].ToolResponses) > iny {
-				toolResponse := messages[inx+1].ToolResponses[iny]
-
-				// Parse the tool response
-				var response map[string]interface{}
-				err = json.Unmarshal([]byte(toolResponse.Content), &response)
-				if err != nil {
-					return nil, err
-				}
-
-				// Add the tool response to the history
-				history = append(history, &genai.Content{
-					Role: RoleUser,
-					Parts: []genai.Part{genai.FunctionResponse{
-						Name:     call.Function.Name,
-						Response: response,
-					}},
-				})
-			}
-		}
-	}
-
-	return history, nil
-}
-
 func (client *Client) Completion(ctx context.Context, req *llm.CompletionRequest) (*llm.CompletionResponse, error) {
 	if len(req.Messages) == 0 {
 		return nil, errors.New("no messages")
@@ -95,7 +35,7 @@ func (client *Client) Completion(ctx context.Context, req *llm.CompletionRequest
 	chat := model.StartChat()
 
 	// Transform the messages to a history
-	history, err := client.transformToHistory(req.Messages)
+	history, err := transformToHistory(req.Messages)
 	if err != nil {
 		return nil, err
 	}
