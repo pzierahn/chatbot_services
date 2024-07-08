dd5c341afe36af5e0c049422762ba5727887052b
patrick <patrick.zierahn@student.kit.edu>
2024-07-08 11:41:28 +0200

    Added bson

--- llm/chat.go
+++ llm/chat.go
@@ -48,76 +48,76 @@ type ToolDefinition struct {
 // Function defines the function name and arguments
 type Function struct {
 	// Name of the function to call
-	Name string `json:"name,omitempty"`
+	Name string `json:"name,omitempty" bson:"name,omitempty"`
 
 	// Arguments to pass to the function
-	Arguments string `json:"arguments,omitempty"`
+	Arguments string `json:"arguments,omitempty" bson:"arguments,omitempty"`
 }
 
 // ToolCall defines which tool to call
 type ToolCall struct {
 	// ID of the tool call
-	CallID string `json:"tool_call_id,omitempty"`
+	CallID string `json:"tool_call_id,omitempty" bson:"call_id,omitempty"`
 
 	// Define function to call
-	Function Function `json:"function,omitempty"`
+	Function Function `json:"function,omitempty" bson:"function,omitempty"`
 }
 
 // ToolResponse defines the response from the tool
 type ToolResponse struct {
 	// Calling tool ID
-	CallID string `json:"tool_call_id,omitempty"`
+	CallID string `json:"tool_call_id,omitempty" bson:"tool_call_id,omitempty"`
 
 	// Tool response
-	Content string `json:"content,omitempty"`
+	Content string `json:"content,omitempty" bson:"content,omitempty"`
 }
 
 // Message defines a message in the thread
 type Message struct {
 	// Role of the message
-	Role string `json:"role,omitempty"`
+	Role string `json:"role,omitempty" bson:"role,omitempty"`
 
 	// User or assistant message
-	Content string `json:"content,omitempty"`
+	Content string `json:"content,omitempty" bson:"content,omitempty"`
 
 	// Tool calls by assistant
-	ToolCalls []ToolCall `json:"tool_calls,omitempty"`
+	ToolCalls []ToolCall `json:"tool_calls,omitempty" bson:"tool_calls,omitempty"`
 
 	// Tool calls response by tool
-	ToolResponses []ToolResponse `json:"tool_responses,omitempty"`
+	ToolResponses []ToolResponse `json:"tool_responses,omitempty" bson:"tool_responses,omitempty"`
 }
 
 // CompletionRequest defines the request to the completion API
 type CompletionRequest struct {
 	// SystemPrompt is the prompt for the system
-	SystemPrompt string `json:"system_prompt,omitempty"`
+	SystemPrompt string `json:"system_prompt,omitempty" bson:"system_prompt,omitempty"`
 
 	// Messages in the thread
-	Messages []*Message `json:"messages,omitempty"`
+	Messages []*Message `json:"messages,omitempty" bson:"messages,omitempty"`
 
 	// Model to use for completion
-	Model string `json:"model,omitempty"`
+	Model string `json:"model,omitempty" bson:"model,omitempty"`
 
 	// MaxTokens is the maximum number of tokens to generate
-	MaxTokens int `json:"max_tokens,omitempty"`
+	MaxTokens int `json:"max_tokens,omitempty" bson:"max_tokens,omitempty"`
 
 	// TopP is the nucleus sampling probability
-	TopP float32 `json:"top_p,omitempty"`
+	TopP float32 `json:"top_p,omitempty" bson:"top_p,omitempty"`
 
 	// Temperature is the sampling temperature
-	Temperature float32 `json:"temperature,omitempty"`
+	Temperature float32 `json:"temperature,omitempty" bson:"temperature,omitempty"`
 
 	// UserId to prevent abuse
-	UserId string `json:"user_id,omitempty"`
+	UserId string `json:"user_id,omitempty" bson:"user_id,omitempty"`
 }
 
 // CompletionResponse defines the response from the completion API
 type CompletionResponse struct {
 	// Message to return
-	Message *Message `json:"message,omitempty"`
+	Message *Message `json:"message,omitempty" bson:"message,omitempty"`
 
 	// Usage of the model
-	Usage ModelUsage `json:"usage,omitempty"`
+	Usage ModelUsage `json:"usage,omitempty" bson:"usage,omitempty"`
 }
 
 type Chat interface {
