2eca9b6e66e8333a494574df6443a97694e8cf71
patrick <patrick.zierahn@student.kit.edu>
2024-07-09 10:11:37 +0200

    Added comments

--- account/account.go
+++ account/account.go
@@ -7,11 +7,15 @@ import (
 	pb "github.com/pzierahn/chatbot_services/proto"
 )
 
+// Verifier is an interface for verifying user identity and funding.
 type Verifier interface {
+	// Verify check if the context contains valid user credentials. If successful, the user ID is returned.
 	Verify(context.Context) (userId string, err error)
+	// VerifyFunding checks if the context contains valid user credentials and the user has enough funding to perform an action.
 	VerifyFunding(context.Context) (userId string, err error)
 }
 
+// Service is the account service implementation.
 type Service struct {
 	pb.UnimplementedAccountServiceServer
 	Database *datastore.Service
